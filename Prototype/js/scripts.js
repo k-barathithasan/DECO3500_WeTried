d6e0f34e99719d79a13eaf11558ecc6d5ed85647
k-barathithasan <s4906600@student.uq.edu.au>
2025-09-22 14:21:30 +1000

    update_login_page

---
+++ Prototype/js/scripts.js
@@ -0,0 +1,20 @@
+document.getElementById("loginForm").addEventListener("submit", function(event){
+  event.preventDefault();
+
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+  let message = document.getElementById("message");
+
+  if(username === "admin" && password === "1234") {
+    message.style.color = "var(--color-accent-green)";
+    message.textContent = "Login successful! Redirecting...";
+
+    // Wait 1 second before redirecting
+    setTimeout(() => {
+      window.location.href = "html/home.html";
+    }, 1000);
+  } else {
+    message.style.color = "var(--color-accent-orange)";
+    message.textContent = "Invalid username or password.";
+  }
+});
